cac1980bdfcac882b13dec0cae890a6d6449eb7e
malakehab2003 <lokaehab2003@gmail.com>
2024-07-01 01:06:46 +0300

    fix task 2

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import router from './routes/index';
-import bodyParser from 'body-parser';
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/', router);
 
